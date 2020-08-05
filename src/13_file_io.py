7783c7cee5b2c278d8bf268fc4e9a7f6355f7807
Adonis Puente <adonispuente@MacBook-Pro.attlocal.net>
2020-08-05 13:40:32 -0400

    completed mvp

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,11 +9,18 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
-# YOUR CODE HERE
-
+with open('foo.txt', 'r') as f:
+    print(f.read())
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('bar.text', 'w') as f:
+    f.write('My name is Adonis\n')
+    f.write('My favorite artist is Russ\n')
+    f.write('Lambda School student\n')
+
+with open('bar.text', 'r') as f:
+    print(f.read())
